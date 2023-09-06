ccf8db54a9618f4915f8763dddf47d81610da36a
Juan Alfonsus Napitupulu <juanalfonsusn@gmail.com>
2023-09-06 21:32:53 +0700

    fix: about me page

--- app/src/views/AboutMeView.jsx
+++ app/src/views/AboutMeView.jsx
@@ -1,7 +1,6 @@
 import Foto from '../assets/foto.png'
 import BlueCircle from '../components/BlueCircle'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import 'animate.css'
 
 export default function AboutMeView() {
     return (
@@ -13,25 +12,33 @@ export default function AboutMeView() {
                     {/* <h1 className=" font-sans">About Me</h1> */}
                     <div className="grid grid-cols-2">
                         <div className=" flex justify-center">
-                            <img src={Foto} alt="foto" className="" />
+                            <img src={Foto} alt="foto" className="animate__animated animate__fadeInLeft" />
                         </div>
                         <div className="">
-                            <div className='text-center'>
+                            <div className='text-center animate__animated animate__fadeInDown'>
                                 <h1 className=' font-lilito text-6xl text-gray-600'>Juan Alfonsus</h1>
                                 <h1 className=' font-lilito text-3xl text-gray-500'>Fullstack Developer</h1>
                             </div>
-                            <div className='mt-6 text-justify text-xl'>
+                            <div className='mt-6 text-justify text-xl animate__animated animate__fadeInUp'>
                                 <p className='z-10'>I hooked at programming when I was in a high school. And began my programming journey at
                                     Gunadarma University, where I studied Informatics Engineering. After graduated, I continued my
                                     programming education by completing Hacktiv8 bootcamp as a full-stack javascript developer. I
                                     always eager to learn new things and seeking a challenging position in the tech industry, where I can
                                     use my skills and passion to make a positive impact.</p>
                             </div>
-                            <div className='flex justify-center mt-10 gap-10'>
-                            <i class="fa-brands fa-linkedin text-4xl" style={{ color: '#253e6a' }}></i>
-                            <i class="fa-brands fa-github text-4xl" style={{ color: '#253e6a' }}></i>
-                            <i class="fa-brands fa-facebook text-4xl" style={{ color: '#253e6a' }}></i>
-                            <i class="fa-brands fa-instagram text-4xl" style={{ color: '#253e6a' }}></i>
+                            <div className='flex justify-center mt-10 gap-10 animate__animated animate__fadeInUp'>
+                                <a href="https://www.linkedin.com/in/juannalfonsus/" target='_blank'>
+                                    <i class="fa-brands fa-linkedin text-4xl" style={{ color: '#253e6a' }}></i>
+                                </a>
+                                <a href="https://github.com/JuanAlfonsus" target='_blank'>
+                                    <i class="fa-brands fa-github text-4xl" style={{ color: '#253e6a' }}></i>
+                                </a>
+                                <a href="https://www.facebook.com/" target='_blank'>
+                                    <i class="fa-brands fa-facebook text-4xl" style={{ color: '#253e6a' }}></i>
+                                </a>
+                                <a href="https://www.instagram.com/juannalfonsus" target='_blank'>
+                                    <i class="fa-brands fa-instagram text-4xl" style={{ color: '#253e6a' }}></i>
+                                </a>
                             </div>
                         </div>
                     </div>
